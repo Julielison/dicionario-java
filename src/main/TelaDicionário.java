71d2e95e97f481711e702f10f9f4deb35dfdd319
Julielison <julielisonlima11@gmail.com>
2025-01-26 22:41:13 -0300

    interface definida

--- src/main/TelaDicionário.java
+++ src/main/TelaDicionário.java
@@ -19,11 +19,20 @@ import javax.swing.JLabel;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
 import javax.swing.SwingConstants;
+import javax.swing.JList;
+import javax.swing.ListSelectionModel;
+import javax.swing.AbstractListModel;
+import javax.swing.JSpinner;
+import javax.swing.JTextPane;
+import javax.swing.DropMode;
+import javax.swing.JScrollPane;
+import javax.swing.JScrollBar;
+import java.awt.Font;
 
 public class TelaDicionário {
 
-	private JFrame Dicionário;
-	private JTextField textField;
+	private JFrame frameDicionário;
+	private JTextField palavraParaTraduzir;
 
 	/**
 	 * Launch the application.
@@ -33,7 +42,7 @@ public class TelaDicionário {
 			public void run() {
 				try {
 					TelaDicionário window = new TelaDicionário();
-					window.Dicionário.setVisible(true);
+					window.frameDicionário.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
@@ -52,68 +61,54 @@ public class TelaDicionário {
 	 * Initialize the contents of the frame.
 	 */
 	private void initialize() {
-		Dicionário = new JFrame();
-		Dicionário.setBounds(100, 100, 450, 300);
-		Dicionário.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		Dicionário.getContentPane().setLayout(null);
+		frameDicionário = new JFrame();
+		frameDicionário.setBounds(100, 100, 450, 300);
+		frameDicionário.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frameDicionário.getContentPane().setLayout(null);
+		frameDicionário.setResizable(false);
 
-		textField = new JTextField();
-		textField.setBounds(34, 91, 180, 76);
-		Dicionário.getContentPane().add(textField);
-		textField.setColumns(10);
-
-		JButton btnNewButton = new JButton("Localizar");
-		btnNewButton.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
-			}
-		});
-		btnNewButton.setBounds(70, 173, 105, 27);
-		Dicionário.getContentPane().add(btnNewButton);
 		
 		JLabel labelBandeira1 = new JLabel("");
 		labelBandeira1.setIcon(new ImageIcon(TelaDicionário.class.getResource("/imagens/Português.png")));
 		labelBandeira1.setBounds(89, 12, 60, 41);
-		Dicionário.getContentPane().add(labelBandeira1);
+		frameDicionário.getContentPane().add(labelBandeira1);
 
 		JLabel labelBandeira2 = new JLabel("");
 		labelBandeira2.setIcon(new ImageIcon(TelaDicionário.class.getResource("/imagens/Inglês.png")));
 		labelBandeira2.setBounds(278, 12, 60, 41);
-		Dicionário.getContentPane().add(labelBandeira2);
+		frameDicionário.getContentPane().add(labelBandeira2);
 		
 		ArrayList<JLabel> labelsBandeiras = new ArrayList<JLabel>(Arrays.asList(labelBandeira1, labelBandeira2));
 
-		JComboBox<String> comboBox = new JComboBox<String>();
-		comboBox.addActionListener(new ActionListener() {
+		JComboBox<String> comboBoxTraducao = new JComboBox<String>();
+		comboBoxTraducao.setFont(new Font("Dialog", Font.BOLD, 14));
+		comboBoxTraducao.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
-				definirBandeiras(comboBox, labelsBandeiras);
+				definirBandeiras(comboBoxTraducao, labelsBandeiras);
 			}
 		});
 		
-		comboBox.setModel(new DefaultComboBoxModel<String>(
+		comboBoxTraducao.setModel(new DefaultComboBoxModel<String>(
 				new String[] { "Português    >    Inglês", "Português    >    Espanhol", "Português    >    Alemão",
 						"Inglês    >    Português", "Espanhol    >    Português", "Alemão    >    Português" }));
 		
-		definirBandeiras(comboBox, labelsBandeiras);
+		definirBandeiras(comboBoxTraducao, labelsBandeiras);
 		
 		// Criando um renderizador personalizado
 		DefaultListCellRenderer renderizador = new DefaultListCellRenderer();
 		renderizador.setHorizontalAlignment(SwingConstants.CENTER); // Centraliza o texto
-		comboBox.setRenderer(renderizador);
+		comboBoxTraducao.setRenderer(renderizador);
 
-		comboBox.setBounds(34, 53, 364, 26);
-		Dicionário.getContentPane().add(comboBox);
-
-		JTextArea textArea = new JTextArea();
-		textArea.setBounds(229, 91, 169, 76);
-		Dicionário.getContentPane().add(textArea);
+		comboBoxTraducao.setBounds(34, 53, 364, 26);
+		frameDicionário.getContentPane().add(comboBoxTraducao);
 
 		JLabel lblIdiomaCorrente = new JLabel("Tradutor");
 		lblIdiomaCorrente.setBounds(188, 24, 60, 17);
-		Dicionário.getContentPane().add(lblIdiomaCorrente);
+		frameDicionário.getContentPane().add(lblIdiomaCorrente);
 
 		JLabel feedback = new JLabel("");
-		feedback.setBounds(25, 212, 383, 27);
-		Dicionário.getContentPane().add(feedback);
+		feedback.setBounds(34, 212, 364, 27);
+		frameDicionário.getContentPane().add(feedback);
 
 		JButton btnTraduzir = new JButton("Traduzir");
 		btnTraduzir.addActionListener(new ActionListener() {
@@ -121,8 +116,37 @@ public class TelaDicionário {
 			}
 		});
 
-		btnTraduzir.setBounds(260, 173, 105, 27);
-		Dicionário.getContentPane().add(btnTraduzir);
+		btnTraduzir.setBounds(293, 173, 105, 27);
+		frameDicionário.getContentPane().add(btnTraduzir);
+		
+		palavraParaTraduzir = new JTextField();
+		palavraParaTraduzir.setFont(new Font("Dialog", Font.PLAIN, 16));
+		palavraParaTraduzir.setToolTipText("");
+		palavraParaTraduzir.setBounds(35, 91, 182, 76);
+		frameDicionário.getContentPane().add(palavraParaTraduzir);
+		palavraParaTraduzir.setColumns(10);
+		
+		JScrollPane scrollPane = new JScrollPane();
+		scrollPane.setBounds(229, 91, 169, 76);
+		frameDicionário.getContentPane().add(scrollPane);
+		
+		JTextArea resultadoTraduçãoLocalização = new JTextArea();
+		resultadoTraduçãoLocalização.setFont(new Font("Dialog", Font.PLAIN, 16));
+		resultadoTraduçãoLocalização.setEditable(false);
+		scrollPane.setViewportView(resultadoTraduçãoLocalização);
+		
+		JButton btnNewButton = new JButton("Localizar");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				
+			}
+		});
+		btnNewButton.setBounds(34, 173, 105, 27);
+		frameDicionário.getContentPane().add(btnNewButton);
+		
+		JButton btnLimpar = new JButton("Limpar");
+		btnLimpar.setBounds(165, 173, 105, 27);
+		frameDicionário.getContentPane().add(btnLimpar);
 	}
 
 	
